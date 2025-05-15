cb8c5b61cc0fad789c8caf12fcf41a717f9436a7
Pynthamil Pavendan <pyndu15@gmail.com>
2025-05-15 15:02:58 +0530

    Convert frontend from submodule to regular directory

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-3xl text-red-100 font-bold underline'>
+        Hello World!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
